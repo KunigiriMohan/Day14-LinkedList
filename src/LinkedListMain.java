a914fcc292536269d653e006798943fa4ace2ed8
KunigiMohan <kmohanr5@gmail.com>
2021-11-07 13:22:04 +0530

    Added : Adding 30 & 56 to 70 in Linked List.

--- src/LinkedListMain.java
+++ src/LinkedListMain.java
@@ -2,10 +2,13 @@ public class LinkedListMain {
 
     public static void main(String[] args) {
         LinkedList list = new LinkedList();         //Creating object of LinkedList class
-        list.push(56);
-        list.push(30);
         list.push(70);
 
+        System.out.println("Before Adding");
+        list.show();
+        System.out.println("After Adding");
+        list.add(30);
+        list.add(56);
         list.show();
     }
 }
