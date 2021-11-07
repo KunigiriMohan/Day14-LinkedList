0e0009c94de0a805cff5cfa683da50b25b6db345
KunigiMohan <kmohanr5@gmail.com>
2021-11-07 13:12:25 +0530

    Added : Adding Linked list file containing methods

---
+++ src/LinkedList.java
@@ -0,0 +1,33 @@
+class LinkedList {
+
+    Node head;
+    Node tail;
+/*
+* push() method to push elements to Nodes in linked list
+* */
+    public Node push(int data) {
+        Node newNode = new Node(data);      //Creating new node
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        return newNode;
+    }
+/*
+* show() method to show elements present in Linked List
+* */
+    public void show() {
+        Node temp = head;
+        if (head == null) {
+            System.out.println("Linked List is Empty");
+        } else {
+            while (temp != null) {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
