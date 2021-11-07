e131494f8d49f130574e0b81f4f0d6de4ca86b35
KunigiMohan <kmohanr5@gmail.com>
2021-11-07 13:14:55 +0530

    Added : Creating Linked List of 56,30 and 70

---
+++ src/LinkedListMain.java
@@ -0,0 +1,11 @@
+public class LinkedListMain {
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();         //Creating object of LinkedList class
+        list.push(56);
+        list.push(30);
+        list.push(70);
+
+        list.show();
+    }
+}
