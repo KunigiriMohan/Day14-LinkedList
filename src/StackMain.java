8ef24c6c89a33a986257deba82d2ac16a603a3d9
KunigiMohan <kmohanr5@gmail.com>
2021-11-19 10:13:04 +0530

    Added : Inserting data in Linked List in Queue order.

--- src/StackMain.java
+++ src/StackMain.java
@@ -2,24 +2,13 @@ public class StackMain {
 
     public static void main(String[] args){
         Stack obj =new Stack();             //Creating object of Stack class
-        obj.push(70);                   //pushing the data into LinkedList
-        obj.push(30);
-        obj.push(56);
+        obj.enQueUe(56);                //Inserting data in LinkedList in queue
+        obj.enQueUe(30);
+        obj.enQueUe(70);
 
-        System.out.println("Elements Present in LInked List in Stack order are :");
-        obj.printStack();                               //Calling printStack() method to print element present in Linked List
-        System.out.println();
+        System.out.println("Elements in LinkedList in Queue");
 
-        obj.peak();                                             //Calling peak and pop method for peak and pop operations
-        System.out.println("Elements after peak");
-        obj.printStack();
-        System.out.println();
-        obj.pop();
-        System.out.println("Elements after  first pop ");
-        obj.printStack();
-        System.out.println();
-        obj.pop();
-        System.out.println("Elements after second pop ");
-        obj.printStack();
+        obj.printStack();               //printing data present inLinkedList in Queue order
+
     }
 }
